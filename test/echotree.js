037fa0a232efdb218e126b66852457523ffeb9a1
juzejian <juzejian@gmail.com>
2023-11-18 14:24:05 +0000

    update

--- test/echotree.js
+++ test/echotree.js
@@ -67,7 +67,51 @@ def App():
   )
 `
 
-const code = code3
+const code4 = `
+from fryhcs import Element, html
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get('/')
+def index():
+    return html(App, title="My Editor")
+
+def App():
+    return (
+    <div ref=(root) h-100vh text-lg>
+      <div foo>
+      </div>
+      <div foo>
+      </div>
+    </div>
+    <script root>
+      import {EditorView, basicSetup} from "codemirror"
+      import {keymap} from "@codemirror/view"
+      import {indentWithTab} from "@codemirror/commands"
+      import {fryhcs as language} from "codemirror-lang-fryhcs"
+      //import {html as language} from "@codemirror/lang-html"
+      //import {python as language} from "@codemirror/lang-python"
+      //import {javascript as language} from "@codemirror/lang-javascript"
+
+      const theme = EditorView.theme({
+          "cm-content": { height: "100%",
+                "font-family": 'Consolas, "Courier New", monospace'},
+      })
+      let editor = new EditorView({
+          extensions: [
+            basicSetup,
+            keymap.of([indentWithTab]),
+            language(),
+            theme,
+          ],
+          parent: root,
+      })
+    </script>)
+`
+
+const code = code4
 
 const parser = fryhcsLanguage.parser
 const tree = parser.parse(code)
