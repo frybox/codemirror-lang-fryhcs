97ca258f2c1d3b9a255a20ac35b207bf81bb757b
juzejian <juzejian@gmail.com>
2023-11-18 13:03:55 +0000

    update

--- test/echotree.js
+++ test/echotree.js
@@ -1,6 +1,6 @@
 const {fryhcsLanguage} = await import("../dist/index.js")
 
-const code = `
+const code1 = `
 from fryhcs import Element, html
 
 def index():
@@ -16,6 +16,58 @@ def App():
       root.innerHTML = "你好，fryhcs！"
     </script>)
 `
+
+const code2 = `
+from fryhcs import Element, html
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+  return html(App, title="My Editor")
+
+def App():
+  return (
+    <div ref=(root)>
+      你好
+    </div>
+    <script root init={25}>
+      import {EditorView, }
+  )
+`
+
+const code3 = `
+from fryhcs import Element, html
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+  return html(App, title="My Editor")
+
+def App():
+  return (
+    <div ref=(root)>
+      你好
+    </div>,
+    <script root init={25}>
+      import {EditorView, basicSetup} from "codemirror"
+      import {fryhcs} from "codemirror-lang-fryhcs"
+
+      const editor = new EditorView({
+        extensions: [
+          basicSetup,
+          fryhcs(),
+        ],
+        parent: root,
+      })
+    </script>
+  )
+`
+
+const code = code3
 
 const parser = fryhcsLanguage.parser
 const tree = parser.parse(code)
